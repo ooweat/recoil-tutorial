348e0242c43144d0b53b7eed44e0addfe1b3b92b
chris_kim <twkim326@gmail.com>
2024-05-27 10:17:25 +0900

    체크박스 전체선택 기능 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,31 @@
-import { useRecoilValue } from 'recoil'
+import {useRecoilValue, useRecoilState} from 'recoil'
 import * as state from '../state/todos'
 
 import Todo from './Todo'
 
 function Main() {
-  const todos = useRecoilValue(state.filteredTodos) //atom 구독
-  const Todos = todos.map(todo => <Todo key={todo.id} todo={todo} />)
+  const filteredTodos = useRecoilValue(state.filteredTodos)
+  const Todos = filteredTodos.map(todo => <Todo key={todo.id} todo={todo}/>)
+
+  const [todos, setTodos] = useRecoilState(state.todos)
+  const isAllDone = todos.every(todo => todo.done)
+  const handleToggleAll = e => {
+    const {checked} = e.target
+    setTodos(todos => todos.map(todo => ({...todo, done: checked})))
+  }
 
   return (
       <section className="main">
-        <input id="toggle-all" className="toggle-all" type="checkbox" />
+        <input id="toggle-all" className="toggle-all" type="checkbox"
+               checked={isAllDone}
+               onChange={handleToggleAll}
+        />
         <label htmlFor="toggle-all">Mark all as complete</label>
         <ul className="todo-list">
-          { Todos }
+          {Todos}
         </ul>
       </section>
   )
 }
-
 
 export default Main
