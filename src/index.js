c1fc348230b4350030493d72570451df4df708c9
chris_kim <twkim326@gmail.com>
2024-05-27 10:52:46 +0900

    react18 정책 변경에 따른 react-dom 구현 변경

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { RecoilRoot } from 'recoil'
+import {createRoot} from 'react-dom/client'
+import {RecoilRoot} from 'recoil'
 import App from './App'
 import 'todomvc-app-css/index.css'
 
-ReactDOM.render(
-    <React.StrictMode>
-      <RecoilRoot>
-        <App />
-      </RecoilRoot>
-    </React.StrictMode>,
-    document.getElementById('root')
+const root = createRoot(document.getElementById('root'))
+root.render(
+    <RecoilRoot>
+      <App/>
+    </RecoilRoot>
 )
